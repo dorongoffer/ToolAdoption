bcb3d4f088cf4fc3eeac54018b25a4427068b4b9
Doron Goffer <doron.goffer@wework.com>
2019-09-08 23:38:11 +0300

    nicer prints

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from util import global_config
 from deepmerge import always_merger
 
 if __name__ == '__main__':
-    config_file_path = 'conf/random_world_simulation.json'
+    config_file_path = 'conf/my_first_world.json'
     with open(config_file_path, 'r') as f:
         config = json.load(f)
         always_merger.merge(global_config.config, config)
@@ -15,8 +15,12 @@ if __name__ == '__main__':
         world = World.from_config(config['world'])
         events = config["generational_events"]
         simulator = Simulator(world, generational_events=events)
-        print("Running simulation with config {}: {}"
+
+        print("Running simulation with config {}:\n{}"
               .format(config_file_path, json.dumps(global_config.get_vars(), indent=2)))
+        print()
+
         print("Initial world: %s" % world)
+        print()
         simulator.run()
         print("Final world: %s" % world)
